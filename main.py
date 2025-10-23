c8302319eecd642d9d30b9d7926a34e7a89a2eff
tell1to <hmd.murillo.edu.ec>
2025-10-23 11:17:23 -0500

    Pliss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from funciones import sumar
 
-a = int(input("Ingresa el primer número: "))
+a = int(input("Ingresa el primer número por favor: "))
 b = int(input("Ingresa el segundo número: "))
 
 resultado = sumar(a, b)
