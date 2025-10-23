1457dc72a0578174129454768284c5c2f562b235
tell1to <hmd.murillo.edu.ec>
2025-10-23 11:14:58 -0500

    control-de-error

--- test_unitario.py
+++ test_unitario.py
@@ -1,10 +1,10 @@
 from funciones import sumar
 
 def test_suma_basica():
-    assert sumar(2, 3) == 5
+    assert sumar(2, 2) == 5
 
 def test_suma_negativos():
-    assert sumar(-1, -1) == -2
+    assert sumar(-1, -2) == -2
 
 
 
